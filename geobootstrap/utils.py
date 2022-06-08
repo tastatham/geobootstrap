91730e07c2e6daac94199b7738eabce66f569a08
tastatham <tastatham@gmail.com>
2022-06-08 17:00:51 +0100

    update sample point method

--- geobootstrap/utils.py
+++ geobootstrap/utils.py
@@ -1,36 +1,18 @@
 import numpy as np
-import pandas as pd
 import geopandas as gpd
 
 
-def _get_coords(
-    gdf,
-    method="mid points",
-    uid=None,
-    bounds=None,
-    p=1000,
-    n=1,
-    join=False,
-):
+def _get_coords(gdf, method="random", size=1, batch_size=1000):
     """
     Get array containing x,y coordinates from GeoDataFrame
-
     Parameters
     ----------
     gdf : gpd.GeoDataFrame
 
-    method : str
-        method for generating coordinates for each polygon
-    uid : str
-        GeoDataFrame unique identifier
-    bounds : array_like
-        GeoDataFrame bounds
-    p : int (default: 1000)
-        number of random points to generate within bounds
-    n : int (default: 1)
+    size : int (default: 1)
         number of random points to generate within polygon (default 1)
-    join : bool (default: False)
-        whether to spatially join original data back
+    bath_size : int (default: 1000)
+        number of random points to generate within bounds
 
     Returns
     -------
@@ -38,218 +20,69 @@ def _get_coords(
         array containing x and y values
     """
 
+    geom_type = gdf.type.values
+
     if "x" and "y" in gdf:
         return gdf[["x", "y"]].values
 
     else:
-        geom_types = gdf.geom_type
-
-        if len(geom_types.unique()) > 1:
-            raise ValueError("Geobootstrap does not support mixed geometry types")
-
-        geom_type = geom_types[0].values
-
-        if geom_type == "Point":
+        if geom_type.any() == "Point":
             x, y = gdf.geometry.x, gdf.geometry.y
-
             return np.array([x, y]).T
 
-        elif geom_type == "Polygon":
-            print("Using mid points, with 1000 points as default")
-            gdf = _poly_to_points(gdf)
-            x, y = gdf.geometry.x, gdf.geometry.y
+        elif geom_type.any() in ("Polygon", "MultiPolygon"):
+            if method == "random":
+                return sample_points(gdf, size, batch_size)
+            else:
+                x_mids, y_mids = _calculate_mid_points(gdf.bounds)
+                return np.array([x_mids, y_mids]).T
 
-            return np.array([x, y]).T
-
-        else:
-            raise ValueError("Geobootstrap only supports Point and Polygon types")
+        elif geom_type.any() in ("LineString", "MultiLineString"):
+            raise ValueError("Not suppported")
 
 
-def _poly_to_points(
-    gdf,
-    method="mid points",
-    uid=None,
-    bounds=None,
-    p=1000,
-    n=1,
-    join=False,
-):
+def sample_points(gdf, size, batch_size):
     """
-    Transforms polygon geometries to point ones using a method
-
-    Parameters
-    ----------
-    gdf : gpd.GeoDataFrame
-        GeoDataFrame containing polygons
-    method : str
-        method for generating coordinates for each polygon
-    uid : str
-        GeoDataFrame unique identifier
-    bounds : array_like
-        GeoDataFrame bounds
-    p : int
-        number of random points to generate within bounds
-    n : int
-        number of random points to generate within polygon (default 1)
-    join :
-        whether to spatially join original data back
-
-    Returns
-    ---------
-    type : gpd.GeoDataFrame
+    modified version of ljw's st_sample PR:
+    https://github.com/geopandas/geopandas/pull/2363
     """
 
-    if "x" and "y" not in gdf:
-        print(
-            f"x and y attributes not in GeoDataFrame, \
-            estimating coordinates using {method}"
+    geoms = gdf.geometry.apply(_uniform_polygon, size=size)
+
+    g = gpd.GeoDataFrame(geometry=geoms, crs=gdf.crs)
+
+    g["x"] = g.geometry.x
+    g["y"] = g.geometry.y
+
+    return g[["x", "y"]].values
+
+
+def _uniform_polygon(geom, size=1, batch_size=None):
+    """
+    modified version of ljw st_sample PR - random method only
+    https://github.com/geopandas/geopandas/pull/2363
+    """
+    from geopandas.array import points_from_xy
+    from geopandas.geoseries import GeoSeries
+
+    n_points = size
+
+    if batch_size is None:
+        batch_size = n_points
+
+    xmin, ymin, xmax, ymax = geom.bounds
+
+    candidates = []
+
+    while len(candidates) < n_points:
+        batch = points_from_xy(
+            x=np.random.uniform(xmin, xmax, size=batch_size),
+            y=np.random.uniform(ymin, ymax, size=batch_size),
         )
+        valid_samples = batch[batch.sindex.query(geom, predicate="contains")]
+        candidates.extend(valid_samples)
 
-        gdf_points = _get_points(gdf, method, bounds, n, p)
-
-    else:
-        gdf_points = gpd.GeoDataFrame(
-            geometry=gpd.points_from_xy(gdf["x"], gdf["y"]), crs=gdf.crs
-        )
-
-    if join is True:
-        gdf = gpd.sjoin(gdf_points, gdf, op="intersects")
-        return gdf
-
-    else:
-        return gdf_points
-
-
-def _get_points(gdf, method, bounds, n=None, p=None):
-    """
-    Get GeoDataFrame point coordinates from GeoDataFrame using a defined method
-
-    Parameters
-    ----------
-    gdf : gpd.GeoDataFrame
-        GeoDataFrame to get point coordinates from
-    method : str
-        method
-    bounds : array_like
-        bounds of a GeoDataFrame
-    n : int
-        number of random points to generate
-    p : int
-        number of random points to generate points within bounds
-    Returns
-    -------
-    type : gpd.GeoDataFrame
-        GeoDataFrame containing point coordinates
-    """
-
-    gdf = gdf.copy()
-
-    if "x" and "y" not in gdf:
-        print(
-            f"x and y not in GeoDataFrame, \
-            estimating coordinates using {method}"
-        )
-
-        if bounds is None:
-            bounds = gdf.bounds.to_numpy()
-
-        if method == "mid points":
-
-            x_mids, y_mids = _calculate_mid_points(bounds)
-            points = np.array([x_mids, y_mids]).T
-            df = pd.DataFrame(points, columns=["x", "y"])
-
-            return gpd.GeoDataFrame(
-                df, geometry=gpd.points_from_xy(x_mids, y_mids), crs=gdf.crs
-            )
-
-        elif method == "random points":
-            """
-            if n or p is None:
-                raise ValueError("n and p must be specified
-                with random points")
-            """
-            gdf_points = _random_points_sample(gdf, bounds, p=p, n=n)
-
-        else:
-            raise ValueError(
-                "Only 'mid points' and \
-                'random points' are supported"
-            )
-
-        return gdf_points
-
-
-def _random_points_sample(gdf, bounds=None, n=1, p=1000):
-    """
-    Gets n random points for every polygon in a GeoDataFrame
-
-    Parameters
-    ----------
-    gdf : gpd.GeoDataFrame
-        GeoDataFrame containing polygons
-    bounds : array_like
-        GeoDataFrame bounds
-    n : int
-        number of random points to generate
-    p : int
-        number of random points to generate points within bounds
-
-    Returns
-    ---------
-    type : gpd.GeoDataFrame
-    """
-
-    # if gdf does not contain polygons:
-    #    raise TypeError("")
-
-    gdfs = []
-
-    if bounds is None:
-        bounds = gdf.bounds.to_numpy()
-
-    for i in range(len(gdf)):
-        g = gdf.iloc[[i]]
-        # Initially create p random points based on bounds
-        points = _random_points(bounds[i], p)
-        df = pd.DataFrame(data=points, columns=["x", "y"])
-        gdf_point = gpd.GeoDataFrame(
-            df, geometry=gpd.points_from_xy(df["x"], df["y"]), crs=gdf.crs
-        )
-
-        # Then clip the points to the polygon boundary
-        gdf_point = gdf_point.clip(g)
-        # And sample n points within polygon
-        gdf_point = gdf_point.sample(n)
-        # Set index
-        gdf_point = gdf_point.set_index(g.index)
-        gdfs.append(gdf_point)
-
-    return pd.concat(gdfs)
-
-
-def _random_points(bounds, p):
-    """
-    Calculate p random points based on GeoDataFrame bounds
-
-    Parameters
-    ----------
-    bounds : array_like
-        bounds of GeoDataFrame
-    p : int
-        number of random points to generate points within bounds
-
-    Returns
-    -------
-    type : np.array
-        array containing random points
-    """
-    return np.array(
-        [
-            np.random.uniform(bounds[0], bounds[2], p),
-            np.random.uniform(bounds[1], bounds[3], p),
-        ]
-    ).T
+    return GeoSeries(candidates[:n_points]).unary_union
 
 
 def _calculate_mid_points(bounds):
